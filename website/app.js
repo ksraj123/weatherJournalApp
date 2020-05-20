579b33e03577ef926fab91d107f55a3244a32b25
ksraj123 <kumarsaurabhraj.sr@gmail.com>
2020-05-20 08:10:19 +0530

    functional version ready

--- website/app.js
+++ website/app.js
@@ -4,11 +4,39 @@ const generateBtn = document.getElementById('generate');
 const apiKey = '55e902a33c27ef254437897e8888f021';
 const dimmer = document.querySelector('.dimmer');
 const form = document.querySelector('form');
+const entriesList = document.querySelector('#entries');
 const country = 'us';
 
+// generates a new date string
 function getNewDate(){
     let d = new Date();
     return d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+}
+
+// helper function for addEntriesFunction
+function getEntryFragment(data){
+    const fragment = document.createDocumentFragment();
+    const classes = ['date', 'temp', 'content'];
+    for (let i = 0; i < classes.length; i++){
+        const div = document.createElement('div');
+        div.classList.add(classes[i]);
+        div.innerText = data[i];
+        fragment.appendChild(div);
+    }
+    return fragment;
+}
+
+// adds entries to the Most Recent Entries section 
+function addEntries(entries){
+    entriesList.style.display = "none";
+    entriesList.innerHTML = "";
+    entries.forEach((entry)=>{
+        let newEntry = document.createElement('div');
+        newEntry.classList.add('entryHolder');
+        newEntry.appendChild(getEntryFragment([entry.date, entry.temp, entry.feelings]));
+        entriesList.appendChild(newEntry);
+    });
+    entriesList.style.display = "block";
 }
 
 const getTemp = async (evt)=>{
@@ -33,6 +61,7 @@ const getTemp = async (evt)=>{
       });
       const resJson = await response.json();
       console.log(resJson);
+      addEntries(resJson.posts);
     }  catch(error) {
       console.log("error", error);
       // appropriately handle the error
@@ -41,3 +70,12 @@ const getTemp = async (evt)=>{
   }
 
 form.addEventListener('submit', getTemp);
+
+// makes a GET request to fetch the entries from our server when page loads
+const getEntries = async ()=>{
+    const res = await fetch('/data');
+    const entries = await res.json();
+    addEntries(entries.posts);
+}
+
+window.addEventListener('load', getEntries);
